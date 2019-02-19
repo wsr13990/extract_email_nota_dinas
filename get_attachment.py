17ae879563ced970b27e15cb826b43a6eafd14b5
wsr13990 <wahyusejatiroso@yahoo.co.id>
2019-02-19 11:36:38 +0700

    Add nota dinas column in summary, so it will check new product duplicate by nodin and not by subject

--- get_attachment.py
+++ get_attachment.py
@@ -13,7 +13,7 @@ import pandas as pd
 import datetime
 
 FILE_REKAP = 'New Products Assmt.xlsx'
-TEST = False
+MOVE_EML = False
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
@@ -23,61 +23,62 @@ def get_eml_body(eml_file):
         text = msg.get_body(preferencelist=('plain')).get_content()
         return text
 
-def get_subject(eml_file):
+def get_eml_subject(eml_file):
     with open(eml_file, 'rb') as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
         return msg.get_all('Subject')[0]
 
-def get_nota_dinas(eml_file):
-    return get_subject(eml_file).split("(")[-1].split(")")[-2]
+def get_eml_nota_dinas(eml_file):
+    return get_eml_subject(eml_file).split("(")[-1].split(")")[-2]
 
-def download_attachment(eml_file,dest_path,move_eml=True):
+def download_eml_attachment(eml_file,dest_path,move_eml=True):
     is_success = False
     attachment_counter = 0
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(eml_file, 'rb') as fp:
-            msg = BytesParser(policy=policy.default).parse(fp)
-            count_attachments = len(msg.get_payload())
-            if count_attachments > 0 :
-                for i in range(1,len(msg.get_payload())):
-                    attachment = msg.get_payload()[i]
-                    attachment_name = attachment.get_filename()
-                    open(attachment_name,'wb').write(attachment.get_payload(decode=True))                
-                    if os.path.isdir(dest_path):
-                        shutil.move(os.path.join(dir_path, attachment_name),
-                                    os.path.join(dest_path, attachment_name))
-                        attachment_counter += 1
-            fp.close()
-            if count_attachments > 0 :
-                if attachment_counter == count_attachments-1:
-                    is_success = True
-                if is_success and move_eml == True:
-                    shutil.move(os.path.join(dir_path, eml_file), os.path.join(dest_path, eml_file))
-                     
-
-def main():
+        msg = BytesParser(policy=policy.default).parse(fp)
+        count_attachments = len(msg.get_payload())
+        if count_attachments > 0 :
+            for i in range(1,len(msg.get_payload())):
+                attachment = msg.get_payload()[i]
+                attachment_name = attachment.get_filename()
+                open(attachment_name,'wb').write(attachment.get_payload(decode=True))                
+                if os.path.isdir(dest_path):
+                    shutil.move(os.path.join(dir_path, attachment_name),
+                                os.path.join(dest_path, attachment_name))
+                    attachment_counter += 1
+        fp.close()
+        if count_attachments > 0 :
+            if attachment_counter == count_attachments-1:
+                is_success = True
+            if is_success and move_eml == True:
+                shutil.move(os.path.join(dir_path, eml_file), os.path.join(dest_path, eml_file))
+                  
+def main_eml():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     rekap = pd.read_excel(FILE_REKAP,sheet_name='Sheet1')
     for eml_file in files:
         if eml_file.endswith('.eml'):
-            subject = get_subject(eml_file)
-            if len(rekap[rekap['Title / Subject'].str.strip()==subject]) >= 1:
-#                print('file already processed')
+            subject = get_eml_subject(eml_file)
+            nodin = get_eml_nota_dinas(eml_file)
+            if len(rekap[rekap['Nota Dinas'].str.strip()==nodin]) >= 1:
                 os.remove(eml_file)
             else:         
                 num = max(rekap['No'], default=0) + 1
-                folder = get_nota_dinas(eml_file).replace('/','').replace('.','')
+                folder = get_eml_nota_dinas(eml_file).replace('/','').replace('.','')
                 dest_path = os.path.join(dir_path,str(num)+'. '+folder)
                 if os.path.isdir(dest_path) == False:
                     os.mkdir(dest_path)
                 if os.path.isdir(dest_path):
-                    if TEST == True:
-                        download_attachment(eml_file,dest_path,False)
+                    if MOVE_EML == True:
+                        download_eml_attachment(eml_file,dest_path,False)
                     else:
-                        download_attachment(eml_file,dest_path)
-                rekap = rekap.append({'No' : num , 'Title / Subject' : subject,
+                        download_eml_attachment(eml_file,dest_path)
+                rekap = rekap.append({'No' : num ,'Nota Dinas': nodin, 'Title / Subject' : subject,
                                       'Tanggal Terima' : datetime.datetime.now(),
                                       'Assessment Status' : 'not started'} , ignore_index=True)
             rekap.to_excel(FILE_REKAP,index=False)
+            
+##TODO: Add .msg functionality
                 
-main()
+main_eml()
