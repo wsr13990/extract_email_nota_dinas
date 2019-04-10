7cd0646ba4b7eac1c84845d3e8539865e18c1e82
wsr13990 <wahyusejatiroso@yahoo.co.id>
2019-04-10 12:49:15 +0700

    Change in functionality so, this script will look for n previous email instead of days

--- get_attachment_direct.py
+++ get_attachment_direct.py
@@ -13,15 +13,16 @@ import datetime as dt
 
 FILE_REKAP = 'New Products Assmt.xlsx'
 TXT_FILE = 'rekap_nodin.txt'
-FOLDER='new_product'
-PREV_DAY=100
+FOLDER = 'new_product'
+NUM_PREV_EMAIL = 10
+
 
 class OutlookLib:
     def __init__(self, settings={}):
         self.settings = settings
 
-    # Gets all messages in outlook   
-    def get_messages(self,folder=None, n = 3):      
+    # Gets all messages in outlook
+    def get_messages(self, folder=None):
         outlook = win32com.client.Dispatch("Outlook.Application")
 
         # This allows us to access the "folder" hierarchy accessible within
@@ -29,16 +30,18 @@ class OutlookLib:
         # manually and bringing up the folder menu
         # (which typically says "Inbox" or "Outlook Today" or something).
         ns = outlook.GetNamespace("MAPI")
-        
+
         if folder == None:
             messages = ns.GetDefaultFolder(6).Items
         else:
             messages = ns.GetDefaultFolder(6).Folders(FOLDER).Items
         messages.Sort("[ReceivedTime]", True)
-        last_n_day = dt.datetime.now() - dt.timedelta(days = n)
-        last_n_day = last_n_day.strftime('%m/%d/%Y %H:%M %p') 
-        messages = messages.Restrict("[ReceivedTime] >= '" + last_n_day +"'")
-        print(messages)
+        # help(messages.GetLast)
+        # last_n_day = dt.datetime.now() - dt.timedelta(days=n)
+        # last_n_day = last_n_day.strftime('%m/%d/%Y')
+        # restriction = "[ReceivedTime] >= '" + last_n_day + "'"
+        # messages = messages.Restrict(restriction)
+        # print(messages.GetFirst())
         return messages
 
     def get_body(self, msg):
@@ -46,16 +49,15 @@ class OutlookLib:
 
     def get_subject(self, msg):
         subject = msg.Subject
-        subject = subject.replace("FW: ","")
-        subject = subject.replace("Fwd: ","")
+        subject = subject.replace("FW: ", "")
+        subject = subject.replace("Fwd: ", "")
         return subject
-    
+
     def get_nodin(self, msg):
         nodin = msg.Subject
         try:
             nodin = nodin.split("(")[-1].split(")")[-2]
-        except IndexError as e:
-            print('Failed to get nodin number due to invalid character')
+        except IndexError:
             nodin = 'NODIN_ERROR'
         return nodin
 
@@ -73,43 +75,50 @@ def Main():
     global attach
 
     outlook = OutlookLib()
-    messages = outlook.get_messages(FOLDER,PREV_DAY)
+    messages = outlook.get_messages(FOLDER)
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     # Loop all messages
     msg = messages.GetFirst()
-    if msg == None:
-        print('Failed to read email due to unknown error, try change PREV_DAY variable to larger value')
-    while msg:
-        rekap = pd.read_excel(FILE_REKAP,sheet_name='Sheet1')
-        nodin = outlook.get_nodin(msg)
-        subject = outlook.get_subject(msg)
-        folder = outlook.get_nodin(msg).replace('/','').replace('.','')
-        if len(rekap[rekap['Nota Dinas'].str.strip()==nodin]) >= 1:
-            pass
+    for _ in range(NUM_PREV_EMAIL):
+        if msg == None:
+            print('Failed to read email due to unknown error')
         else:
-            num = max(rekap['No'], default=0) + 1
-            dest_path = os.path.join(dir_path,str(num)+'. '+folder)
-            if os.path.isdir(dest_path) == False:
-                os.mkdir(dest_path)
-            if os.path.isdir(dest_path):
-                attach = []
-                if not len(msg.Attachments) is 0:
-                    attach.append((msg.Attachments, msg.Subject))
-                for attachTuple in attach:
-                    print("Downloading attachments under " + attachTuple[1])
-                    for fileAtt in attachTuple[0]:
-                        print(dest_path+'/'+fileAtt.FileName)
-                        fileAtt.SaveAsFile(dest_path+'/'+ fileAtt.FileName)
-                rekap = rekap.append({'No' : num ,'Nota Dinas': nodin, 'Title / Subject' : subject,
-                          'Tanggal Terima' : datetime.datetime.now(),
-                          'Assessment Status' : 'not started'} , ignore_index=True)
-        msg = messages.GetNext()
-        print('Done downloading '+nodin)
-        rekap.to_excel(FILE_REKAP,index=False)
-        rekap['Folder'] = rekap['Nota Dinas'].map(lambda x: x.replace('.',''))
-        rekap['Folder'] = rekap['Folder'].map(lambda x: x.replace('/',''))
-        rekap[['Folder','Nota Dinas','Title / Subject','Tanggal Terima']].to_csv(TXT_FILE,sep='|', index=False, header=True)
+            # while msg:
+            rekap = pd.read_excel(FILE_REKAP, sheet_name='Sheet1')
+            nodin = outlook.get_nodin(msg)
+            subject = outlook.get_subject(msg)
+            folder = outlook.get_nodin(msg).replace('/', '').replace('.', '')
+            if len(rekap[rekap['Nota Dinas'].str.strip() == nodin]) >= 1:
+                pass
+            else:
+                num = max(rekap['No'], default=0) + 1
+                dest_path = os.path.join(dir_path, str(num)+'. '+folder)
+                if os.path.isdir(dest_path) == False:
+                    os.mkdir(dest_path)
+                if os.path.isdir(dest_path):
+                    attach = []
+                    if not len(msg.Attachments) is 0:
+                        attach.append((msg.Attachments, msg.Subject))
+                    for attachTuple in attach:
+                        print("Downloading attachments under " +
+                              attachTuple[1])
+                        for fileAtt in attachTuple[0]:
+                            print(dest_path+'/'+fileAtt.FileName)
+                            fileAtt.SaveAsFile(
+                                dest_path+'/' + fileAtt.FileName)
+                    rekap = rekap.append({'No': num, 'Nota Dinas': nodin, 'Title / Subject': subject,
+                                          'Tanggal Terima': datetime.datetime.now(),
+                                          'Assessment Status': 'not started'}, ignore_index=True)
+            msg = messages.GetNext()
+            print('Done downloading '+nodin)
+            rekap.to_excel(FILE_REKAP, index=False)
+            rekap['Folder'] = rekap['Nota Dinas'].map(
+                lambda x: x.replace('.', ''))
+            rekap['Folder'] = rekap['Folder'].map(lambda x: x.replace('/', ''))
+            rekap[['Folder', 'Nota Dinas', 'Title / Subject', 'Tanggal Terima']
+                  ].to_csv(TXT_FILE, sep='|', index=False, header=True)
+
 
 if __name__ == "__main__":
     Main()
